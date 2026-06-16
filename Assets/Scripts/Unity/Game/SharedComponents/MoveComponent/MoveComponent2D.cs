7549cee08b727febc5e53a6862dcb5d7c44e46fa
maxim.alekseev <supermax143@gmail.com>
2026-06-16 14:06:33 +0300

    Added base 2D behavior.

--- Assets/Scripts/Unity/Game/SharedComponents/MoveComponent/MoveComponent2D.cs
+++ Assets/Scripts/Unity/Game/SharedComponents/MoveComponent/MoveComponent2D.cs
@@ -6,13 +6,17 @@ namespace Unity.Game
     {
         public override void RotateTo(Vector3 targetPosition)
         {
-            var direction = targetPosition - transform.position;
+            /*var direction = targetPosition - transform.position;
             direction.y = 0;
             if (direction != Vector3.zero)
             {
                 float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
                 transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-            }
+            }*/
+            var scale = transform.localScale;
+            scale.x = Mathf.Sign(targetPosition.x - transform.position.x);
+            transform.localScale = scale;
+            
         }
     }
 }
