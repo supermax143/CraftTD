bf8e376c0c87845c304273cea8b1c31e1b2c5e5f
maxim.alekseev <supermax143@gmail.com>
2026-07-01 12:09:44 +0300

    refactor

--- Assets/Scripts/Core/Application/Models/Inventory/Item.cs
+++ Assets/Scripts/Core/Application/Models/Inventory/Item.cs
@@ -1,21 +1,31 @@
 using System;
+using UnityEngine;
 
 namespace Core.Application.Models
 {
     [Serializable]
     public class Item
     {
-        public string Id;
-        public ItemType Type;
-        public string Label;
-        public string Description;
+        [SerializeField] private string _id;
 
+        [SerializeField] private ItemType _type;
+
+        [SerializeField] private string _label;
+
+        [SerializeField] private string _description;
+
+        public string Id => _id;
+        public ItemType Type => _type;
+        public string Label => _label;
+        public string Description => _description;
+
+        
         public Item(string id, ItemType type, string label, string description)
         {
-            Id = id;
-            Type = type;
-            Label = label;
-            Description = description;
+            _id = id;
+            _type = type;
+            _label = label;
+            _description = description;
         }
     }
 }
