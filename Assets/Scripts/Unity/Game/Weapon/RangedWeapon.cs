51f8c7f5e632453e86b8a46b815696ba88ea683c
maxim.alekseev <supermax143@gmail.com>
2026-07-09 13:37:07 +0300

    base rifle animation

--- Assets/Scripts/Unity/Game/Weapon/RangedWeapon.cs
+++ Assets/Scripts/Unity/Game/Weapon/RangedWeapon.cs
@@ -12,8 +12,23 @@ namespace Unity.Game.Projectile
         
         public override void Attack(AttackTargetBase target, float damage, Vector3 direction)
         {
-            var projectile = Instantiate(_projectilePrefab, _barrel.position, _barrel.rotation);
-            projectile.GetComponent<ProjectileComponent>().Launch(target, damage);
+            if (_projectilePrefab == null)
+            {
+                ApplyDamage(target, damage);
+            }
+            else
+            {
+                var projectile = Instantiate(_projectilePrefab, _barrel.position, _barrel.rotation);
+                projectile.GetComponent<ProjectileComponent>().Launch(target, damage);
+            }
+        }
+
+        private void ApplyDamage(AttackTargetBase target, float damage)
+        {
+            if (target != null && target.HealthComponent != null)
+            {
+                target.HealthComponent.TakeDamage(damage);
+            }
         }
     }
 }
