8e9b72bc466b3423571ad40291a37c2ad816dd0d
maxim.alekseev <supermax143@gmail.com>
2026-07-29 14:55:46 +0300

    removed unused

--- Assets/Scripts/Unity/Game/Unit/Behavior/DeathState.cs
+++ Assets/Scripts/Unity/Game/Unit/Behavior/DeathState.cs
@@ -29,48 +29,12 @@ namespace Unity.Game
             rb.linearVelocity = Vector2.zero;
             rb.AddForce(hitDirection.normalized * knockbackForce, ForceMode2D.Impulse);
 
-            DOVirtual.DelayedCall(1.5f, () => _unit.Die());
-            //PlayBounceAnimation(hitDirection);
+            DOVirtual.DelayedCall(1.5f, () => _unit.Die(), false);
         }
 
         public override void UpdateState()
         {
             _unit.View.UpdateSortingByPosition();
-        }
-
-        private void PlayBounceAnimation(Vector2 direction)
-        {
-            visual.DOKill();
-
-            float sign = Mathf.Sign(direction.x);
-
-            Sequence seq = DOTween.Sequence();
-
-            // Первый большой прыжок
-            seq.Append(visual.DOLocalMoveY(0.35f, 0.12f).SetEase(Ease.OutQuad));
-            seq.Append(visual.DOLocalMoveY(0f, 0.12f).SetEase(Ease.InQuad));
-
-            // Второй
-            seq.Append(visual.DOLocalMoveY(0.18f, 0.09f).SetEase(Ease.OutQuad));
-            seq.Append(visual.DOLocalMoveY(0f, 0.09f).SetEase(Ease.InQuad));
-
-            // Третий
-            seq.Append(visual.DOLocalMoveY(0.08f, 0.07f).SetEase(Ease.OutQuad));
-            seq.Append(visual.DOLocalMoveY(0f, 0.07f).SetEase(Ease.InQuad));
-
-           
-
-            // Немного "расплющить" при первом ударе
-            seq.Insert(
-                0.12f,
-                visual.DOScale(new Vector3(1.08f, 0.92f, 1), 0.05f)
-                    .SetLoops(2, LoopType.Yoyo));
-
-            seq.OnComplete(() =>
-            {
-                visual.localPosition = Vector3.zero;
-                DOVirtual.DelayedCall(.7f, () => _unit.Die());
-            });
         }
         
     }
