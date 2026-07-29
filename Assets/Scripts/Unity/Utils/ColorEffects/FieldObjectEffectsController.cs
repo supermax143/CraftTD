8e298c0465094f4443b038e0297b89c8e013f63f
maxim.alekseev <supermax143@gmail.com>
2026-07-29 17:42:12 +0300

    adde effec to vertical dissolve

--- Assets/Scripts/Unity/Utils/ColorEffects/FieldObjectEffectsController.cs
+++ Assets/Scripts/Unity/Utils/ColorEffects/FieldObjectEffectsController.cs
@@ -17,14 +17,17 @@ namespace Utils.ColorEffects
         {
             public static readonly int _Dissolve = Shader.PropertyToID(nameof(_Dissolve));
             public static readonly int _DissolveAmount = Shader.PropertyToID(nameof(_DissolveAmount));
-            public static readonly int _DissolveVertical = Shader.PropertyToID(nameof(_DissolveVertical));
+            public static readonly int _VerticalDissolve = Shader.PropertyToID(nameof(_VerticalDissolve));
+            public static readonly int _VerticalDissolveAmount = Shader.PropertyToID(nameof(_VerticalDissolveAmount));
             public static readonly int _BoundsTop = Shader.PropertyToID(nameof(_BoundsTop));
             public static readonly int _BoundsBottom = Shader.PropertyToID(nameof(_BoundsBottom));
             public static readonly int _HitEffect = Shader.PropertyToID(nameof(_HitEffect));
             public static readonly int _HitAmount = Shader.PropertyToID(nameof(_HitAmount));
         }
 
-        private Timer _animationTimer = new(TimeType.Unscaled);
+        private readonly Timer _hitAnimationTimer = new(TimeType.Scaled);
+        private readonly Timer _dissolveAnimationTimer = new(TimeType.Scaled);
+        private readonly Timer _verticalDissolveAnimationTimer = new(TimeType.Scaled);
         
         private void OnValidate()
         {
@@ -34,29 +37,13 @@ namespace Utils.ColorEffects
         private void Awake()
         {
             _propertyBlock = new MaterialPropertyBlock();
-            /*var boundsTopId = Shader.PropertyToID("_BoundsTop");
-            var boundsBottomId = Shader.PropertyToID("_BoundsBottom");
-            Bounds bounds = _renderers[0].bounds;
-
-
-            foreach (var r in _renderers)
-                bounds.Encapsulate(r.bounds);
-            _propertyBlock.SetFloat(boundsTopId, bounds.max.y);
-            _propertyBlock.SetFloat(boundsBottomId, bounds.min.y);
-            foreach (var renderer in _renderers)
-            {
-                if (renderer != null)
-                {
-                    renderer.SetPropertyBlock(_propertyBlock);
-                }
-            }*/
         }
         
         public IEnumerator ShowDissolveEffect(float time)
         {
-            _animationTimer.Start(time);
+            _dissolveAnimationTimer.Start(time);
             _propertyBlock.SetFloat(ShaderProperties._Dissolve, 1);
-            while (!_animationTimer.IsComplete)
+            while (!_dissolveAnimationTimer.IsComplete)
             {
                 //нужно передавать параметр bound в шейдер для корректного применения к разным спрайтоам одного юнита
                 /*Bounds bounds = _renderers[0].bounds;
@@ -65,21 +52,21 @@ namespace Utils.ColorEffects
                 _propertyBlock.SetFloat(boundsTopId, bounds.max.y);
                 _propertyBlock.SetFloat(boundsBottomId, bounds.min.y);*/
                 
-                _propertyBlock.SetFloat(ShaderProperties._DissolveAmount, _animationTimer.Progress);
+                _propertyBlock.SetFloat(ShaderProperties._DissolveAmount, _dissolveAnimationTimer.Progress);
                 UpdateRenderersPropertyBlock();
                 yield return null;
             }
-            _propertyBlock.SetFloat(ShaderProperties._Dissolve, 0);
+            //_propertyBlock.SetFloat(ShaderProperties._Dissolve, 0);
             UpdateRenderersPropertyBlock();
         }
         
         public IEnumerator ShowHitEffect(float time)
         {
             _propertyBlock.SetFloat(ShaderProperties._HitEffect, 1);
-            _animationTimer.Start(time/2);
-            while (!_animationTimer.IsComplete)
+            _hitAnimationTimer.Start(time/2);
+            while (!_hitAnimationTimer.IsComplete)
             {
-                _propertyBlock.SetFloat(ShaderProperties._HitAmount, _animationTimer.Progress);
+                _propertyBlock.SetFloat(ShaderProperties._HitAmount, _hitAnimationTimer.Progress);
                 UpdateRenderersPropertyBlock();
                 yield return null;
             }
@@ -87,16 +74,47 @@ namespace Utils.ColorEffects
             _propertyBlock.SetFloat(ShaderProperties._HitAmount, 1);
             UpdateRenderersPropertyBlock();
             
-            _animationTimer.Start(time/2);
-            while (!_animationTimer.IsComplete)
+            _hitAnimationTimer.Start(time/2);
+            while (!_hitAnimationTimer.IsComplete)
             {
-                _propertyBlock.SetFloat(ShaderProperties._HitAmount,1 - _animationTimer.Progress);
+                _propertyBlock.SetFloat(ShaderProperties._HitAmount,1 - _hitAnimationTimer.Progress);
                 UpdateRenderersPropertyBlock();
                 yield return null;
             }
             
             _propertyBlock.SetFloat(ShaderProperties._HitAmount, 0);
             _propertyBlock.SetFloat(ShaderProperties._HitEffect, 0);
+            UpdateRenderersPropertyBlock();
+        }
+
+        [ContextMenu("Show VDissolve")]
+        public void ShowVDissolve()
+        {
+            StartCoroutine(ShowVerticalDissolveEffect(2));
+            StartCoroutine(ShowDissolveEffect(2));
+        }
+        
+        public IEnumerator ShowVerticalDissolveEffect(float time)
+        {
+            Bounds bounds = _renderers[0].bounds;
+
+            foreach (var r in _renderers)
+            {
+                bounds.Encapsulate(r.bounds);
+            }
+            _propertyBlock.SetFloat(ShaderProperties._BoundsTop, bounds.max.y);
+            _propertyBlock.SetFloat(ShaderProperties._BoundsBottom, bounds.min.y);
+           
+            _verticalDissolveAnimationTimer.Start(time);
+            _propertyBlock.SetFloat(ShaderProperties._VerticalDissolve, 1);
+            UpdateRenderersPropertyBlock();
+            while (!_verticalDissolveAnimationTimer.IsComplete)
+            {
+                _propertyBlock.SetFloat(ShaderProperties._VerticalDissolveAmount, _verticalDissolveAnimationTimer.Progress);
+                UpdateRenderersPropertyBlock();
+                yield return null;
+            }
+            //_propertyBlock.SetFloat(ShaderProperties._VerticalDissolve, 0);
             UpdateRenderersPropertyBlock();
         }
         
