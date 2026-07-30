a4f76aadf70ee7ef594f1e64d52791cc1d880b9b
maxim.alekseev <supermax143@gmail.com>
2026-07-30 14:20:38 +0300

    cave image changed

--- Assets/Scripts/Unity/Game/Tower/TowerView.cs
+++ Assets/Scripts/Unity/Game/Tower/TowerView.cs
@@ -91,7 +91,7 @@ namespace Unity.Game
                     var maxY = bounds.max.y - (bounds.size.y * timer.Progress);
                     var randomY = Random.Range(bounds.min.y, maxY);
                     var randomPosition = new Vector3(randomX, randomY, transform.position.z);
-                    _effectSpawnManager.SpawnRandomExplosion(randomPosition, transform);
+                    _effectSpawnManager.SpawnRandomExplosion(randomPosition);
                     _cameraController.ShakeCamera();
                     explosionTimer.Start(0.2f);
                 }
