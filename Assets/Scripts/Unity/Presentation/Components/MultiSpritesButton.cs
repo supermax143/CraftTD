18fee48925058f7af26b9033256d88e022275d12
maxim.alekseev <supermax143@gmail.com>
2026-08-06 19:21:29 +0300

    multy sprites button added

---
+++ Assets/Scripts/Unity/Presentation/Components/MultiSpritesButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Unity.Presentation.Components
+{
+    public class MultiSpritesButton : Button
+    {
+        [SerializeField, HideInInspector]
+        private Image[] _images;
+
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+            _images = GetComponentsInChildren<Image>();
+        }
+
+        protected override void DoStateTransition(SelectionState state, bool instant)
+        {
+            foreach (var image in _images)
+            {
+                var color = state == SelectionState.Disabled ? colors.disabledColor : colors.normalColor;
+                image.color = color;
+            }
+            base.DoStateTransition(state, instant);
+        }
+    }
+}
